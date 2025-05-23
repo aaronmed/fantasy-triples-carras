858b36e861aa735f3d253a6d8f2f903f6e4a48e5
aaronmed <aaronmedmel@gmail.com>
2025-05-23 19:00:27 +0100

    Cambio a hash mode en router para GitHub Pages

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import GeneralView from '../views/GeneralView.vue'
 import JornadaView from '../views/JornadaView.vue'
 import TeamsView from '../views/TeamsView.vue'
@@ -12,7 +12,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
 })
 
